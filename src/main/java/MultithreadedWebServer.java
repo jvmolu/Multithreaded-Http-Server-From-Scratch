fc31e787d4dad18a55f82a46703ed7382d0a7d1d
Jvmolu <anmol.singhal50@gmail.com>
2024-06-10 17:41:56 +0530

    test

--- src/main/java/MultithreadedWebServer.java
+++ src/main/java/MultithreadedWebServer.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,8 +14,19 @@ public class MultithreadedWebServer {
     private ExecutorService executorService;
 
     public static String readClientMessage(Socket clientSocket) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        return in.readLine();
+        System.out.println("reading client message");
+        Scanner scanner = new Scanner(clientSocket.getInputStream());
+        StringBuilder request = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            request.append(line).append("\r\n");
+        }
+        scanner.close();
+        System.out.println("client message: " + request.toString());
+        return request.toString();
     }
 
     public MultithreadedWebServer(int port, int num_threads) throws IOException {
