f10450b7a3aa8d41171548567dfbcd49d99e580f
Jvmolu <anmol.singhal50@gmail.com>
2024-06-10 15:53:42 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+  public static String readClientMessage(Socket clientSocket, Integer bytes) throws IOException {
+    byte[] buffer = new byte[bytes];
+    int bytesRead = clientSocket.getInputStream().read(buffer);
+    return new String(buffer, 0, bytesRead);
+  }
+
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -17,6 +24,11 @@ public class Main {
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
+
+      // Get Message from client
+      String clientMessage = readClientMessage(clientSocket, 1024);
+      
+      System.out.println("Message from client: " + clientMessage);
 
       // SEND HTTP/1.1 200 OK\r\n\r\n
       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
